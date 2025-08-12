2d4c6e159879d05c31fabb4598004f375ed62218
Armaan Pal <armaanpal25@gmail.com>
2025-08-12 10:06:01 +0530

    commented all component webinar

--- src/components/Webinar.jsx
+++ src/components/Webinar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import WebHero from './WebinarHero.jsx'
-import WhoThisIsFor from './WhoThisIsFor.jsx';
-import Curriculum from './WebCurriculum.jsx';
-import StartCreatorCareer from './ReadytoStart.jsx';
 import StickyHeader from './Head.jsx';
-import Footer from './Footer.jsx';
-import BonusTracks from './BonusTracks.jsx';
-import FAQSection from './Faq.jsx';
-import YCForm from './WebinarForm.jsx';
+// import WhoThisIsFor from './WhoThisIsFor.jsx';
+// import Curriculum from './WebCurriculum.jsx';
+// import StartCreatorCareer from './ReadytoStart.jsx';
+// import StickyHeader from './Head.jsx';
+// import Footer from './Footer.jsx';
+// import BonusTracks from './BonusTracks.jsx';
+// import FAQSection from './Faq.jsx';
+// import YCForm from './WebinarForm.jsx';
 
 
 const Webinar = () => {
@@ -15,13 +16,13 @@ const Webinar = () => {
         <div>
             <StickyHeader />
             <WebHero />
-            <Curriculum/>
+            {/* <Curriculum/>
             <BonusTracks/>
             <YCForm/>
             <WhoThisIsFor/>
             <StartCreatorCareer/>
             <FAQSection/>
-            <Footer/>
+            <Footer/> */}
         </div>
     )
 }
