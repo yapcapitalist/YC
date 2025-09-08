7e58b44afaa2f5e942751cdfeb780e1b0bcc20a8
Armaan Pal <armaanpal25@gmail.com>
2025-09-08 12:44:09 +0530

    changed number

--- src/components/Refund-Cancelation.jsx
+++ src/components/Refund-Cancelation.jsx
@@ -17,8 +17,8 @@ const RefundCancelation = () => {
             <div>
                 <p> <span className='!font-extrabold !text-[#fefe00] mb-4'>1-</span> If you are not satisfied after two sessions, we offer a 100% full refund—no questions asked.</p>
                 <p><span className='!font-bold !text-[#fefe00] mb-4'>2-</span> Refund requests must be submitted in writing within 7 days of the second session.</p>
-                <p><span className='!font-bold !text-[#fefe00] mb-4'>2-</span>Once your refund request has been submitted, we’ll process it within 7–14 business days.</p>
-                <p><span className='!font-bold !text-[#fefe00] mb-4'>3-</span> Beyond this point, refunds are not available, but we’ll do our best to support you with alternatives.</p>
+                <p><span className='!font-bold !text-[#fefe00] mb-4'>3-</span> Once your refund request has been submitted, we’ll process it within 7–14 business days.</p>
+                <p><span className='!font-bold !text-[#fefe00] mb-4'>4-</span> Beyond this point, refunds are not available, but we’ll do our best to support you with alternatives.</p>
             </div>
         </div>
     )
