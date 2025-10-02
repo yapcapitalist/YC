9bd527195041fcedc2ea242ce04b2ec99078f0d4
Armaan Pal <armaanpal25@gmail.com>
2025-10-02 18:33:13 +0530

    new source

--- src/components/MasterClass.jsx
+++ src/components/MasterClass.jsx
@@ -9,7 +9,7 @@ import GradientButton from "./ui/GradientButton";
 
 
 export default function Master() {
-  const embedUrl = "https://drive.google.com/file/d/1ikvZfoeIYqfQuY8rgHlJnqpRnqohI1Cg/preview";
+  // const embedUrl = "https://drive.google.com/file/d/1ikvZfoeIYqfQuY8rgHlJnqpRnqohI1Cg/preview";
 
   const gotoWaitlist = () => {
     const scrollToTarget = () => {
@@ -42,13 +42,8 @@ export default function Master() {
       <section className="w-full bg-black flex-col flex justify-center items-center py-10 pb-16 sm:pb-[158px] px-4 sm:px-0 max-w-6xl mx-auto">
         <h2 className="font-bold !text-[#fefe00] mb-12 text-center">
           Masterclass </h2>
-        <div className="w-full max-w-5xl aspect-video border-4 border-[#fefe00] rounded-2xl">
-          <iframe
-            src={embedUrl}
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-            className="w-full h-full rounded-2xl shadow-lg border-0"
-          ></iframe>
+        <div className="w-full max-w-5xl aspect-video border-4 border-[#fefe00] rounded-2xl master-frame">
+          <iframe width="auto" height="auto" src="https://www.youtube.com/embed/vQFbUbpQmD8?si=X4kTgdhVcg5vvItd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
       </section>
 
